693c5c78294fbac90d3041991e39ec5c390fcbdb
Firaol Terefe <firaolterefe447@gmail.com>
2026-06-27 18:58:52 +0300

    update

--- accounts/admin.py
+++ accounts/admin.py
@@ -450,6 +450,8 @@ class CustomUserAdmin(SecurityAuditMixin, ModelAdmin):
         'is_verified',
         'is_nexus_visible',
         'is_selected',
+        'is_hero_avatar_selected',
+        'is_home_profile_selected',
         'is_active',
     ]
 
@@ -458,6 +460,8 @@ class CustomUserAdmin(SecurityAuditMixin, ModelAdmin):
         'is_verified',
         'is_nexus_visible',
         'is_selected',
+        'is_hero_avatar_selected',
+        'is_home_profile_selected',
         'is_active'
     ]
 
