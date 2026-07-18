cfbaeef9d52695dac6a2c63b660631cd6f784d09
Firaol Terefe <firaolterefe447@gmail.com>
2026-07-18 12:01:41 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -199,12 +199,15 @@ GOOGLE_OAUTH_CLIENT_SECRET = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET", "")
 GOOGLE_OAUTH_REDIRECT_URI = os.getenv("GOOGLE_OAUTH_REDIRECT_URI", "")
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv("EMAIL_HOST", "")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", "465"))
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = "support@corelink.et"
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD", "")
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True").lower() == "true"
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+DEFAULT_FROM_EMAIL = "CoreLink Ethiopia <support@corelink.et>"
+SERVER_EMAIL = "support@corelink.et"
+EMAIL_TIMEOUT = 5
 
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
