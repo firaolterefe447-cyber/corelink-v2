7c1e4f9fe2f0d1aab09d1110d180573a5effcf34
Firaol Terefe <firaolterefe447@gmail.com>
2026-07-19 11:08:38 +0300

    update

--- profiles/models/__init__.py
+++ profiles/models/__init__.py
@@ -1,25 +1,5 @@
 from .utils import *
 from .new_unified_profile import *
-from .expert import (
-    ExpertProfile,
-    ExpertHeadline,
-    ExpertSkill,
-    ExpertCredential,
-    ExpertProject,
-    ProjectGalleryImage,
-    ExpertExperience,
-    JobPreference,
-    ExpertThought,
-)
-from .visionary import (
-    VisionaryProfile,
-    Certification,
-    Project,
-    ProjectImage,
-    GrowthLog,
-    LearningTarget,
-    VisionBlock,
-)
 from .company import (
     Company,
     CompanySocialLink,
@@ -32,4 +12,3 @@ from .company import (
     NewsGalleryImage,
     CompanyInvitation,
 )
-from .founder import FounderProfile
