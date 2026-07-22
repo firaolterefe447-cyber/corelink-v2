e2b199d1a119022a9859a90ea81248f1b3972235
Firaol Terefe <firaolterefe447@gmail.com>
2026-07-22 19:51:01 +0300

    Deployment update

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,2 @@
 from django.contrib import admin
-from .models import SiteMediaAsset, SiteTextAsset
-
-@admin.register(SiteMediaAsset)
-class SiteMediaAssetAdmin(admin.ModelAdmin):
-    list_display = ('title', 'zone_slug', 'is_active', 'order')
-    list_filter = ('zone_slug', 'is_active')
-    search_fields = ('title', 'zone_slug')
-    ordering = ('zone_slug', 'order')
-
-@admin.register(SiteTextAsset)
-class SiteTextAssetAdmin(admin.ModelAdmin):
-    list_display = ('key', 'description', 'is_rich_text')
-    search_fields = ('key', 'content')
+# NOTE: SiteMediaAsset and SiteTextAsset admin registrations removed as models are unused
